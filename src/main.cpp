3dd5005e859e8f3d5faf14cc26ab024672d92911
johnnypatterson <jpatterson@centhra.com>
2014-02-13 23:30:21 -0800

    Setup CMake and file system

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//- Standard Library -
+#include <iostream>
+
+//- BEACH JUDGE -
+#include <BeachJudge/Config.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << BEACHJUDGE_VERSION_MAJOR << " " << BEACHJUDGE_VERSION_MINOR << endl;
+	return 0;
+}
