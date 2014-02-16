68a2d2736e47f33a2bbe0b601d44cd5c2957c8d2
David Nuon <david@davidnuon.com>
2014-02-16 13:04:21 -0800

    added some bootstrap things

---
+++ www/inc/navi.php
@@ -0,0 +1,29 @@
+
+
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				
+	<ul class="nav navbar-nav">
+		<li>
+			<a href="#">
+				<strong>
+					beachjudge
+				</strong>
+			</a>
+		</li>
+		<li><a href="#">
+			Leaderboard
+		</a></li>
+		<li><a href="#">
+			Submit
+		</a></li>
+		<li><a href="#">
+			Profile
+		</a></li>
+	</ul>
+			</div>
+		</div>
+	</div>
+</nav>
---
+++ www/login.php
@@ -0,0 +1,34 @@
+<?php include("inc/dochead.php"); ?>
+<?php include("inc/navi.php"); ?>
+
+    <div class="container">
+
+		<div class="row">
+			<div class="col-md-4 col-md-push-4">
+				      <form class="form-signin" role="form">
+
+				<div class="form-group">
+					<h2
+					style="text-align:center">
+					beachjudge
+					</h2>
+				</div>
+
+		<div class="form-group">
+	        <input type="email" class="form-control" placeholder="Email address" required autofocus>
+		</div>
+
+		<div class="form-group">
+			<input type="password" class="form-control" placeholder="Password" required>
+		</div>
+		
+
+		<button type="submit" class="btn btn-default">Submit</button>
+
+     </form>
+			</div>
+		</div>
+
+    </div> <!-- /container -->
+
+<?php include("inc/footer.php"); ?>
