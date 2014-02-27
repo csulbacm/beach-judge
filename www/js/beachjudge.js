3168f9434e560ad89ea1189e9fd275a7fdb8ec41
johnnypatterson <jpatterson@centhra.com>
2014-02-26 18:47:12 -0800

    Restricted question input

--- www/js/beachjudge.js
+++ www/js/beachjudge.js
@@ -1,3 +1,29 @@
+function question(e)
+{
+	var key;
+	var keychar;
+
+	if (window.event)
+	   key = window.event.keyCode;
+	else if (e)
+	   key = e.which;
+	else
+	   return true;
+	keychar = String.fromCharCode(key);
+	keychar = keychar.toLowerCase();
+
+	// control keys
+	if ((key==null) || (key==0) || (key==8) || 
+	    (key==9) || (key==13) || (key==27) )
+	   return true;
+
+	// valid characters
+	else if ((("abcdefghijklmnopqrstuvwxyz0123456789 !@@#$%^&*()-=_+?/.,<>;:\'\"[]{}`~\\|").indexOf(keychar) > -1))
+	   return true;
+	else
+	   return false;
+}
+
 String.prototype.toHHMMSS = function () {
     var sec_num = parseInt(this, 10); // don't forget the second param
     var hours   = Math.floor(sec_num / 3600);
