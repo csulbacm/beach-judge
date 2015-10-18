7c4a05fc0f4827eb164ecd7265b80b1559a5729b
johnnypatterson <johnny.patterson.jr@gmail.com>
2015-10-18 16:00:04 -0700

    Split judge protocol from main

--- src/main.cpp
+++ src/main.cpp
@@ -18,136 +18,19 @@
 
 // beachJudge
 #include <Judge/Judge.h>
+#include <Judge/WS_Judge.h>
 
 using namespace judge;
 using namespace std;
 
 map<string, Session> Session::s_sessionMap = map<string, Session>();
 
-#define MAX_MESSAGE_QUEUE 32
-#define MAX_RESPONSE 2047
-
-struct a_message {
-	void *payload;
-	u16 len;
-};
-
-struct per_session_data__judge {
-	libwebsocket *wsi;
-	u16 ringbuffer_tail;
-	a_message ringbuffer[MAX_MESSAGE_QUEUE];
-	u16 ringbuffer_head;
-
-	struct Service *service;
-	u16 width;
-	u16 height;
-	u64 lastSendMS;
-	char buffer[MAX_RESPONSE + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
-	char *msg;
-	Session *session;
-	User *user;
-};
-
-
-//----------------------------------------------
-//------------- Message Handling ---------------
-
-map<string, void (*)(libwebsocket *wsi,
-	per_session_data__judge *pss, char *msgIn)> g_msgHandlerMap;
-
-void msg_populate(libwebsocket *wsi, per_session_data__judge *pss, char *msgIn)
-{
-	// Populate User Session Data
-	sprintf(pss->msg, ""
-		"\"msg\":\"POP\","
-		"\"name\":\"%s\"",
-		pss->user->username.c_str());
-}
-
-void msg_teamList(libwebsocket *wsi, per_session_data__judge *pss, char *msgIn)
-{
-	// Populate Team Data
-	stringstream users;
-	map<string, User *>::iterator it = User::s_usersByName.begin();
-	map<string, User *>::iterator end = User::s_usersByName.end();
-	char entry[64];
-	while (it != end) {
-		sprintf(entry, "{\"i\":\"%04x\",\"n\":\"%s\"}",
-			it->second->id, it->second->username.c_str());
-		users << entry;
-		++it;
-		if (it != end)
-			users << "\",\"";
-	}
-	sprintf(pss->msg, ""
-		"\"msg\":\"TL\","
-		"\"teams\":[%s]",
-		users.str().c_str());
-}
-
-void msg_createTeam(libwebsocket *wsi, per_session_data__judge *pss, char *msgIn)
-{
-	// Restrict action to judge
-	if (pss->user->isJudge == false) {
-		sprintf(pss->msg, "\"msg\": \"ERR\"");
-		return;
-	}
-
-	//TODO: Define name/password length requirements
-	//TODO: Split username and display name
-	char name[16], p1[16], p2[16];
-	i16 r = sscanf(msgIn, "n=%[a-zA-Z0-9]&p1=%[a-zA-Z0-9]&p2=%[a-zA-Z0-9]", name, p1, p2);
-	if (r != 3) {
-		sprintf(pss->msg, ""
-			"\"msg\":\"CT\","
-			"\"err\":\"I\"");
-		return;
-	}
-	if (strcmp(p1, p2) != 0) {
-		//TODO: memcpy prebuilt errors
-		sprintf(pss->msg, ""
-			"\"msg\":\"CT\","
-			"\"err\":\"P\"");
-		return;
-	}
-
-	// Set name to lower case
-	{
-		i16 l = strlen(name);
-		for (i16 a = 0; a < l; ++a)
-			name[a] = tolower(name[a]);
-	}
-
-	if (User::s_usersByName.count(name) != 0) {
-		sprintf(pss->msg, ""
-			"\"msg\":\"CT\","
-			"\"err\":\"N\"");
-		return;
-	}
-
-	User *newUser = new User(name, p1, false);
-	sprintf(pss->msg, ""
-		"\"msg\":\"CT\","
-		"\"i\":\"%04x\","
-		"\"n\":\"%s\"",
-		newUser->id,
-		newUser->username.c_str());
-}
-
-void populateMsgHandlerMap(map<string, void (*)(libwebsocket *wsi,
-	per_session_data__judge *pss, char *msgIn)> &m)
-{
-	m["POP"] = msg_populate;
-	m["TL"] = msg_teamList;
-	m["CT"] = msg_createTeam;
-}
-
 libwebsocket_context *context;
 volatile int force_exit = 0;
 
 enum lws_protocols {
 	PROTOCOL_HTTP = 0,
-	PROTOCOL_LWS_MIRROR,
+	PROTOCOL_JUDGE,
 	LWS_PROTOCOL_COUNT
 };
 
@@ -509,165 +392,6 @@ try_to_reuse:
 	return 0;
 }
 
-int callback_judge(libwebsocket_context *context,
-	libwebsocket *wsi,
-	libwebsocket_callback_reasons reason,
-	void *user, void *in, size_t len)
-{
-	i16 n;
-	per_session_data__judge *pss = (per_session_data__judge *)user;
-
-	switch (reason) {
-
-	case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION: {
-		i16 n = 0;
-		char buf[256];
-		const unsigned char *c;
-		do {
-			c = lws_token_to_string((lws_token_indexes)n);
-			if (!c) {
-				n++;
-				continue;
-			}
-	
-			if (!lws_hdr_total_length(wsi, (lws_token_indexes)n)) {
-				n++;
-				continue;
-			}
-	
-			lws_hdr_copy(wsi, buf, sizeof buf, (lws_token_indexes)n);
-			if (memcmp(c, "cookie:", 7) == 0) {
-				char sessID[65];
-				sessID[64] = 0;
-				if (sscanf(buf, "JUDGESESSID=%[0-9a-zA-Z]", sessID) == 1) {
-					string sessionID = string(sessID);
-					if (Session::s_sessionMap.count(sessionID) != 0) {
-						pss->session = &Session::s_sessionMap[sessionID];
-						pss->session->Reset();
-						pss->user = pss->session->user;
-					}
-				}
-			}
-
-			n++;
-		} while (c);
-		
-		// Block connections without valid session
-		if (pss->user == 0)
-			return -1;
-
-		break;
-	}
-
-	case LWS_CALLBACK_ESTABLISHED:
-		lwsl_info("callback_judge: LWS_CALLBACK_ESTABLISHED\n");
-		pss->ringbuffer_tail = pss->ringbuffer_head;
-		pss->wsi = wsi;
-
-		// Initialize Session
-		pss->width = 0;
-		pss->height = 0;
-		pss->lastSendMS = 0;
-		pss->msg = pss->buffer + LWS_SEND_BUFFER_PRE_PADDING;
-
-		printf("%p: Connected\n", pss);
-		break;
-
-	case LWS_CALLBACK_CLOSED:
-		printf("%p: Disconnected\n", pss);
-		break;
-
-	case LWS_CALLBACK_PROTOCOL_DESTROY:
-		for (n = 0; n < sizeof pss->ringbuffer / sizeof pss->ringbuffer[0]; n++)
-			if (pss->ringbuffer[n].payload)
-				free(pss->ringbuffer[n].payload);
-		break;
-
-	case LWS_CALLBACK_SERVER_WRITEABLE:
-		while (pss->ringbuffer_tail != pss->ringbuffer_head) {
-			// Input Processing
-			char *msgIn = (char *)pss->ringbuffer[pss->ringbuffer_tail].payload + LWS_SEND_BUFFER_PRE_PADDING;
-			char msgType[5];
-			msgType[4] = 0;
-			sscanf(msgIn, "%4[a-zA-Z]:", msgType);
-			if (g_msgHandlerMap.count(msgType))
-				(*g_msgHandlerMap[msgType])(wsi, pss, msgIn + strlen(msgType) + 1);
-			else
-				sprintf(pss->msg, "\"msg\": \"ERR\"");
-			libwebsocket_write(wsi,
-				(unsigned char *)pss->msg,
-				strlen(pss->msg), LWS_WRITE_TEXT);
-
-			if (pss->ringbuffer_tail == (MAX_MESSAGE_QUEUE - 1))
-				pss->ringbuffer_tail = 0;
-			else
-				pss->ringbuffer_tail++;
-
-			if (((pss->ringbuffer_head - pss->ringbuffer_tail) &
-					(MAX_MESSAGE_QUEUE - 1)) == (MAX_MESSAGE_QUEUE - 15))
-				libwebsocket_rx_flow_allow_all_protocol(
-								 libwebsockets_get_protocol(wsi));
-
-//			lwsl_debug("tx fifo %d\n", (pss->ringbuffer_head - pss->ringbuffer_tail) & (MAX_MESSAGE_QUEUE - 1));
-
-			if (lws_partial_buffered(wsi) || lws_send_pipe_choked(wsi)) {
-				libwebsocket_callback_on_writable(context, wsi);
-				break;
-			}
-			/*
-			 * for tests with chrome on same machine as client and
-			 * server, this is needed to stop chrome choking
-			 */
-#ifdef _WIN32
-			Sleep(1);
-#else
-			usleep(1);
-#endif
-		}
-
-		break;
-
-	case LWS_CALLBACK_RECEIVE:
-		if (((pss->ringbuffer_head - pss->ringbuffer_tail) &
-					(MAX_MESSAGE_QUEUE - 1)) == (MAX_MESSAGE_QUEUE - 1)) {
-			lwsl_err("dropping!\n");
-			goto choke;
-		}
-
-		if (pss->ringbuffer[pss->ringbuffer_head].payload)
-			free(pss->ringbuffer[pss->ringbuffer_head].payload);
-
-		pss->ringbuffer[pss->ringbuffer_head].payload =
-				malloc(LWS_SEND_BUFFER_PRE_PADDING + len +
-							LWS_SEND_BUFFER_POST_PADDING);
-		pss->ringbuffer[pss->ringbuffer_head].len = len;
-		memcpy((char *)pss->ringbuffer[pss->ringbuffer_head].payload +
-						LWS_SEND_BUFFER_PRE_PADDING, in, len + 1);
-		if (pss->ringbuffer_head == (MAX_MESSAGE_QUEUE - 1))
-			pss->ringbuffer_head = 0;
-		else
-			pss->ringbuffer_head++;
-
-		if (((pss->ringbuffer_head - pss->ringbuffer_tail) &
-					(MAX_MESSAGE_QUEUE - 1)) != (MAX_MESSAGE_QUEUE - 2))
-			goto done;
-
-choke:
-		lwsl_debug("LWS_CALLBACK_RECEIVE: throttling %p\n", wsi);
-		libwebsocket_rx_flow_control(wsi, 0);
-
-//		lwsl_debug("rx fifo %d\n", (pss->ringbuffer_head - pss->ringbuffer_tail) & (MAX_MESSAGE_QUEUE - 1));
-done:
-		libwebsocket_callback_on_writable_all_protocol(
-								 libwebsockets_get_protocol(wsi));
-		break;
-
-	default:
-		break;
-	}
-
-	return 0;
-}
 
 libwebsocket_protocols protocols[] = {
 	{ // HTTP Handler
@@ -678,8 +402,8 @@ libwebsocket_protocols protocols[] = {
 	},
 	{ // beachJudge Protocol
 		"judge-protocol",
-		callback_judge,
-		sizeof(per_session_data__judge),
+		ws_judge,
+		sizeof(psd_judge),
 		128,
 	},
 	{ NULL, NULL, 0, 0 } /* terminator */
@@ -696,9 +420,6 @@ void sigHandler(int signo, siginfo_t *info, void *context) {
 int main(int argc, char *argv[])
 {
 	srand(time(NULL));
-
-	// Populate Msg Handler Map
-	populateMsgHandlerMap(g_msgHandlerMap);
 
 	// Initialize LWS
 	lws_set_log_level(0, NULL);
