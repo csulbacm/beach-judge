1e7b9c7d6d4de0b994505fb6270af2c7fafd2b8e
johnnypatterson <johnny.patterson.jr@gmail.com>
2016-08-28 21:42:21 -0700

    Fixed start paths

--- src/node/start.js
+++ src/node/start.js
@@ -4,23 +4,24 @@ var cp = require('child_process');
 
 // Launch Rethink
 {
-	var out = fs.openSync(path.resolve(__dirname, '../build/rethinkdb_out.log'), 'a');
-	var err = fs.openSync(path.resolve(__dirname, '../build/rethinkdb_err.log'), 'a');
-	var exe = path.resolve(__dirname, '../build/external/rethinkdb/rethinkdb.exe');
+	var out = fs.openSync(path.resolve(__dirname, '../../rethinkdb_out.log'), 'a');
+	var err = fs.openSync(path.resolve(__dirname, '../../rethinkdb_err.log'), 'a');
+	var exe = path.resolve(__dirname, '../../external/rethinkdb/rethinkdb.exe');
 	var child = cp.spawn(exe, ['--http-port', 8081], { detached: true, stdio: [ 'ignore', 'ignore', 'ignore' ] });
 
-	var pidFile = path.resolve(__dirname, '../build/rethinkdb.pid');
+	var pidFile = path.resolve(__dirname, '../../rethinkdb.pid');
 	fs.writeFile(pidFile, child.pid, (err) => { if (err) throw err; });
 	child.unref();
 }
 
 // Launch beachJudge
 {
-	var out = fs.openSync(path.resolve(__dirname, '../build/beachjudge_out.log'), 'a');
-	var err = fs.openSync(path.resolve(__dirname, '../build/beachjudge_err.log'), 'a');
-	var index = path.resolve(__dirname, '../build/node/generated/index.js');
-	var exe = path.resolve(__dirname, '../build/external/nodejs/node.exe');
+	var out = fs.openSync(path.resolve(__dirname, '../../beachjudge_out.log'), 'a');
+	var err = fs.openSync(path.resolve(__dirname, '../../beachjudge_err.log'), 'a');
+	var index = path.resolve(__dirname, '../../generated/node/index.js');
+	var exe = path.resolve(__dirname, '../../external/nodejs/node.exe');
 
+	console.log(index);
 	//var child = cp.spawn('cmd.exe', ['/c', 'start', '/min', path.resolve(__dirname, '../tools/run-server.bat')], { detached: true, stdio: [ 'ignore', 'ignore', 'ignore' ] });
 	var child = cp.spawn('cmd.exe', ['/c', 'start', '/min', exe, index], { detached: true, stdio: [ 'ignore', out, err ] });
 	// TODO: Capture errors from script execution and display them to this script's stdout stream
