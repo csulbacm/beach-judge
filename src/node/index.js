673cefe6fa6625ba1f787f4fea65229f31e08e32
tylergoodman <tylergoodman@users.noreply.github.com>
2016-08-28 20:53:40 -0700

    move towards building with node

--- src/node/index.js
+++ src/node/index.js
@@ -6,63 +6,66 @@ require('./generate-js.js');
 
 async.waterfall([
   function connect(callback) {
-  	r.connect(g_config.rethinkdb, callback);
+    r.connect(g_config.rethinkdb, callback);
   },
   function createDatabase(connection, callback) {
-  	//Create the database if needed.
-  	r.dbList().contains(g_config.rethinkdb.db).do(function (containsDb) {
-  		return r.branch(
-		  containsDb,
-		  { created: 0 },
-		  r.dbCreate(g_config.rethinkdb.db)
-		);
-  	}).run(connection, function (err) {
-  		callback(err, connection);
-  	});
+    //Create the database if needed.
+    r.dbList().contains(g_config.rethinkdb.db).do(function(containsDb) {
+      return r.branch(
+        containsDb, {
+          created: 0
+        },
+        r.dbCreate(g_config.rethinkdb.db)
+      );
+    }).run(connection, function(err) {
+      callback(err, connection);
+    });
   },
   function createTable(connection, callback) {
-  	//Create the table if needed.
-  	r.tableList().contains('todos').do(function (containsTable) {
-  		return r.branch(
-		  containsTable,
-		  { created: 0 },
-		  r.tableCreate('todos')
-		);
-  	}).run(connection, function (err) {
-  		callback(err, connection);
-  	});
+    //Create the table if needed.
+    r.tableList().contains('todos').do(function(containsTable) {
+      return r.branch(
+        containsTable, {
+          created: 0
+        },
+        r.tableCreate('todos')
+      );
+    }).run(connection, function(err) {
+      callback(err, connection);
+    });
   },
   function createIndex(connection, callback) {
-  	//Create the index if needed.
-  	r.table('todos').indexList().contains('createdAt').do(function (hasIndex) {
-  		return r.branch(
-		  hasIndex,
-		  { created: 0 },
-		  r.table('todos').indexCreate('createdAt')
-		);
-  	}).run(connection, function (err) {
-  		callback(err, connection);
-  	});
+    //Create the index if needed.
+    r.table('todos').indexList().contains('createdAt').do(function(hasIndex) {
+      return r.branch(
+        hasIndex, {
+          created: 0
+        },
+        r.table('todos').indexCreate('createdAt')
+      );
+    }).run(connection, function(err) {
+      callback(err, connection);
+    });
   },
   function waitForIndex(connection, callback) {
-  	//Wait for the index to be ready.
-  	r.table('todos').indexWait('createdAt').run(connection, function (err, result) {
-  		callback(err, connection);
-  	});
+    //Wait for the index to be ready.
+    r.table('todos').indexWait('createdAt').run(connection, function(err, result) {
+      callback(err, connection);
+    });
   }
-], function (err, connection) {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-		return;
-	
-	}
+], function(err, connection) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+    return;
 
-	require('./server-process.js');
-	require('./generate-css.js');
-	require('./generate-js.js');
-	require('./generate-html.js');
-	require('./http.js');
+  }
 
-//	startExpress(connection);
+  require('./server-process.js');
+  require('./generate-css.js');
+  require('./generate-js.js');
+  require('./generate-html.js');
+  require('./http.js');
+
+  //	startExpress(connection);
 });
--- tools/build.sh
+++ tools/build.sh
@@ -1,3 +1,4 @@
 mkdir build
 cd build
 cmake -G 'Unix Makefiles' ..
+./build/external/nodejs/bin/npm i
